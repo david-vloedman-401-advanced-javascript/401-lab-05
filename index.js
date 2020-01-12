625ee6dcd1f02eef0aee343e317c1d269d86ce14
David Vloedman <djvloedman@gmail.com>
2020-01-12 13:12:10 -0800

    Commit before taking a break and opening PR

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
+const Category = require('./models/categoryModel');
+const util = require('util');
 
 
 require('dotenv').config();
 
+const products = new Product();
+const category = new Category();
 
 
 mongoose.connect(process.env.MONGOOSE_URI, {
@@ -12,4 +17,29 @@ mongoose.connect(process.env.MONGOOSE_URI, {
   useUnifiedTopology: true,
   useFindAndModify: false
 });
+
+
+const productRecord = {
+  name: 'david',
+  amount: 23,
+};
+
+const categoryRecord = {
+
+};
+
+products
+  .create(productRecord)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+
+products
+  .get('david')
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+
+
+
 
